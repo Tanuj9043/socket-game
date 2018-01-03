1a9975f165e208b2a5d29a09b1be7ab460a5b292
TANUJ DRALL <Tanuj9043>
2018-01-03 19:49:21 +0530

    Adding files

---
+++ public/javascript.js
@@ -0,0 +1,191 @@
+$(document).ready(function(){
+	var socket, currentUser, dx, dy;
+
+	currentUser = {};
+	dx = 0.08;
+	dy = 0.08;
+
+	function socketConnected(){
+		currentUser.name = window.prompt("Enter username...");
+		$('title').html(currentUser.name);
+
+		socket.emit('user-joined', currentUser.name);		
+	}
+
+	function userJoined(allUsers){
+		currentUser = allUsers[currentUser.name];
+		$('#main').empty();
+
+		var allKeys = Object.keys(allUsers);
+		for(i = 0; i < allKeys.length; i++){
+			var user = allUsers[allKeys[i]];
+			
+			var tank = $('<div />').html($('#tankTemplate').html()).addClass("tank");
+			tank.find('span[purpose=name]').html(user.name).attr('name', user.name);
+
+			tank.css({
+				top: user.y + "%",
+				left: user.x + "%"
+			});
+
+			if(user.d === 'n'){
+				tank.addClass('north')
+			} else if(user.d === 'e'){
+				tank.addClass('east');
+			} else if(user.d === 'w'){
+				tank.addClass('west');
+			} else if(user.d === 's'){
+				tank.addClass('south');
+			}
+
+			$('#main').append(tank);
+		}
+	}
+
+	function userLeft(user){
+		if(user && user.name){
+			$('span[name=' + user.name + ']').prev().attr('src', '/images/ship-dead.png');
+			if(user.name === currentUser.name){
+				$(document).off("keypress");
+			}
+
+			window.setTimeout(function() {
+				$('span[name=' + user.name + ']').parent().remove();
+				if(user.name === currentUser.name){
+					alert("Game over");
+				}
+			}, 2000);				
+		}
+	}
+
+	function reposition(user){
+		if(user && user.name){
+			var tank = $('span[name=' + user.name + ']').parent();
+			tank.css({
+				top: user.y + "%",
+				left: user.x + "%"
+			});
+
+
+			tank.removeClass('north east west south');
+			if(user.d === 'n'){
+				tank.addClass('north');
+			} else if(user.d === 'e'){
+				tank.addClass('east');
+			} else if(user.d === 'w'){
+				tank.addClass('west');
+			} else if(user.d === 's'){
+				tank.addClass('south');
+			}
+		}
+	}
+
+	function handleKey(){
+		var deltaX = 0, deltaY = 0;
+
+		currentUser.timeOfEvent = Date.now();
+		switch(window.event.which){
+			case 119:
+				currentUser.action = 'reposition';
+				if(currentUser.d === 'n'){
+					deltaY = -dy;
+				}
+				else if(currentUser.d === 'e'){
+					deltaX = dx;
+				}
+				else if(currentUser.d === 'w'){
+					deltaX = -dx;
+				}
+				else if(currentUser.d === 's'){
+					deltaY = dy;
+				} 
+
+				if(currentUser.x + deltaX <= 90 && currentUser.x + deltaX >= 0){
+					currentUser.x += deltaX;
+					window.scrollBy(deltaX*100,0);
+					console.log("moves");
+				}
+
+				if(currentUser.y + deltaY <= 80 && currentUser.y + deltaY >= 0){
+					currentUser.y += deltaY;
+					window.scrollBy(0,deltaY*100);
+				}
+			 	break;
+			case 115:
+				currentUser.action = 'reposition';
+				if(currentUser.d === 'n'){
+					deltaY = -dy;
+				}
+				else if(currentUser.d === 'e'){
+					deltaX = dx;
+				}
+				else if(currentUser.d === 'w'){
+					deltaX = -dx;
+				}
+				else if(currentUser.d === 's'){
+					deltaY = dy;
+				} 
+
+				if(currentUser.x - deltaX <= 90 && currentUser.x - deltaX >= 0){
+					currentUser.x -= deltaX;
+					window.scrollBy(-100*deltaX,0);
+				}
+
+				if(currentUser.y - deltaY <= 80 && currentUser.y - deltaY >= 0){
+					currentUser.y -= deltaY;
+					window.scrollBy(0,-100*deltaY);
+				}
+			 	break;
+			case 97:
+				currentUser.action = 'reposition';
+				if(currentUser.d === 'n'){
+					currentUser.d = 'w';
+				}
+				else if(currentUser.d === 'e'){
+					currentUser.d = 'n';
+				}
+				else if(currentUser.d === 'w'){
+					currentUser.d = 's';
+				}
+				else if(currentUser.d === 's'){
+					currentUser.d = 'e';
+				} 
+				break;
+			case 100:
+				currentUser.action = 'reposition';
+				if(currentUser.d === 'n'){
+					currentUser.d = 'e';
+				}
+				else if(currentUser.d === 'e'){
+					currentUser.d = 's';
+				}
+				else if(currentUser.d === 'w'){
+					currentUser.d = 'n';
+				}
+				else if(currentUser.d === 's'){
+					currentUser.d = 'w';
+				} 
+				break;
+			default:
+				break;
+		}
+
+		socket.emit('msg', currentUser);
+		window.event.preventDefault();
+	}
+
+	// define Init
+	function Init(){
+		socket = io();
+
+		socket.on("connect", socketConnected);
+		socket.on('user-joined', userJoined);
+		socket.on('user-left', userLeft);
+		socket.on('reposition', reposition);
+
+		$(document).on("keypress", handleKey);
+	}
+
+	// Call Init
+	Init();
+}); 
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');//importing libraries from express
+const app = express();
+const socketIo = require('socket.io');
+const port = 8080 || process.env.PORT;
+const http = require('http');
+const httpServer = http.Server(app);
+const ioServer = socketIo(httpServer);
+var allSockets = {}, allUsers = {};
+
+app.use(express.static(__dirname + '/public'));
+
+function userJoined(user){
+	console.log(user + ' joined');
+
+	let d = Math.floor(Math.random() * 4);
+	if(d === 0){
+		d = 'n';
+	}else if(d === 1){
+		d = 'e';
+	}else if(d === 2){
+		d = 'w';
+	}else{
+		d = 's';
+	}
+
+	allSockets[user] = this;
+	allUsers[user] = {
+		name: user,
+		timeOfEvent: 0,
+		x: Math.floor(Math.random() * 90),
+		y: Math.floor(Math.random() * 80),
+		d: d
+	};
+
+	ioServer.emit('user-joined', allUsers);	
+}
+
+function userLeft(){
+	var user = null;
+	var allKeys = Object.keys(allSockets);
+
+	for(i = 0; i < allKeys.length; i++){
+		if(allSockets[allKeys[i]] === this){
+			user = allKeys[i];
+		}
+	}
+
+	console.log(user + ' left.');
+	delete allSockets[user];
+	delete allUsers[user];
+
+    this.broadcast.emit('user-left', user);//gives message to all insted of the one leaving the server
+}
+
+function messageReceived(data){
+	//console.log(data);
+
+	if(allUsers[data.name].timeOfEvent < data.timeOfEvent){
+		allUsers[data.name].timeOfEvent = data.timeOfEvent;
+		if (data.action === 'reposition'){
+			allUsers[data.name].x = data.x;
+			allUsers[data.name].y = data.y;
+			allUsers[data.name].d = data.d;
+
+			ioServer.emit('reposition', data);
+		}
+	}
+}
+
+ioServer.on('connection', function(socket){
+	console.log('New socket connection made');
+
+	socket.on('user-joined', userJoined);
+	socket.on('disconnect', userLeft);	
+	socket.on('msg', messageReceived);	
+});
+
+httpServer.listen(8080, function(){
+	console.log(`Server running on port 8080`);
+});//0 to 1024 ports are reserved.
